7548ec0a17fd30d6288fc65da82459abedb9cc8b
Jeonwoojae <108383150+woojae83@users.noreply.github.com>
2022-09-09 17:47:07 +0900

    Update main.js

--- main.js
+++ main.js
@@ -211,7 +211,7 @@ function creatBossEnemy(){
      let bossenemy = new BossEnemy();//적군 생성(new생성자로 공장에서 물건 찍어내듯이 생성가능)
       bossenemy.init(); //적군 좌표 셋팅
 
- },500)//setInterval(호출하고 싶은 함수,시간)  내가  원하는 시간 내가 원한는 함수 호출
+ },2000)//setInterval(호출하고 싶은 함수,시간)  내가  원하는 시간 내가 원한는 함수 호출
 
 
 }
@@ -252,8 +252,8 @@ function update(){
     if(spaceshipY <= 0){
         spaceshipY = 0
     }
-    if(spaceshipY >= canvas.height-64){
-        spaceshipY = canvas. height-64;
+    if(spaceshipY >= canvas.height){
+        spaceshipY = canvas. height+64;
     }
     
     
